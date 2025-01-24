f1a0b4f2f4a079f1d18e325212f0956814d3fdf7
prajwalpinto <prajwalpinto25@gmail.com>
2025-01-24 19:34:49 -0400

    comment price api

--- app/routes/prices.py
+++ app/routes/prices.py
@@ -15,8 +15,10 @@ def serialize_grocery_item(item):
             "store": item.store.value
     }
 
+# get_prices function gets all prices and creates a json file with the prices for now
 @prices_blueprint.route("/prices", methods=["GET"])
 def get_prices():
     items=GroceryItem.query.all()
     serialized_items = [serialize_grocery_item(item) for item in items]
     track_prices(serialized_items)
+# return serialized data
