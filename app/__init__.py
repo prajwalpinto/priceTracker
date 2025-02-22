6072be341a2508588ed4fa1d2c9a751360a9d877
prajwalpinto <prajwalpinto25@gmail.com>
2025-02-21 20:29:59 -0400

    add cors

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask
 from app.db import db
 from .routes.grocery_item import grocery_blueprint
 from.routes.prices import prices_blueprint
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///grocery.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    CORS(app)
     
     db.init_app(app)
     
